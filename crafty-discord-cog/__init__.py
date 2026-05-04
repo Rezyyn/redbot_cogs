115d2314eefc6a50b3bd024e7cc85fee7c53f9a4
Rezyyn <73084106+Rezyyn@users.noreply.github.com>
2026-05-04 13:00:08 +0100

    Update __init__.py

--- crafty-discord-cog/__init__.py
+++ crafty-discord-cog/__init__.py
@@ -1,4 +1,6 @@
 from .craftyserver import CraftyServer
 
+__red_end_user_data_statement__ = "This cog does not store persistent user data."
+
 async def setup(bot):
     await bot.add_cog(CraftyServer(bot))
